922ee6cffb2dd2df3c0f2cccd0f9d073d8f46277
Evan Klitzke <evan@eklitzke.org>
2016-11-01 20:29:58 -0700

    Make runtests.sh smarter.
    It can now:
     * try to test on py2/py3 automatically
     * run the tests for RPM builds, w/o using pip

--- runtests.sh
+++ runtests.sh
@@ -4,17 +4,46 @@ set -ex
 
 ENVDIR="./test_env"
 
-if [ $# -eq 1 ]; then
-    PYTHONVERSION=$1
-elif [ -z "$PYTHONVERSION" ]; then
-    PYTHONVERSION=python2
+# Run tests using pip; $1 = python version
+run_pip_tests() {
+    virtualenv -p "$1" "${ENVDIR}"
+    trap "rm -rf ${ENVDIR}" EXIT
+
+    . "${ENVDIR}/bin/activate"
+    pip install --upgrade pip
+    pip install pytest
+    py.test tests/
+
+    # clean up the trap
+    rm -rf "${ENVDIR}" EXIT
+    trap "" EXIT
+}
+
+# See if we can run the pip tests with this Python version
+try_pip_tests() {
+    if which "$1" &>/dev/null; then
+        run_pip_tests "$1"
+    fi
+}
+
+# This runs the tests for building an RPM
+run_fedora_tests() {
+    py.test-2 tests/
+    py.test-3 tests/
+}
+
+if [ "$1" = "fedora" ]; then
+    # If the first arg is fedora, don't use Pip
+    run_fedora_tests
+elif [ $# -eq 1 ]; then
+    # Run the tests for a particular version of python
+    run_pip_tests "$1"
+elif [ -n "$PYTHONVERSION" ]; then
+    # Run the tests for $PYTHONVERSION
+    run_pip_tests "$PYTHONVERSION"
+else
+    # Try various places where we might find Python
+    for py in python{,2,3}; do
+        try_pip_tests "$py"
+    done
 fi
-
-trap "rm -rf ${ENVDIR}" EXIT
-
-virtualenv -p "$PYTHONVERSION" "${ENVDIR}"
-. "${ENVDIR}/bin/activate"
-pip install --upgrade pip
-pip install pytest
-py.test tests/
-exit $?
