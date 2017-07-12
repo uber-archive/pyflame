2d8181e40cce0f43b11b0edcdfc3b24ab6fb649d
Evan Klitzke <evan@eklitzke.org>
2017-07-12 14:47:00 -0700

    Update Travis configuration, to fix Python version tests.
    This makes the Python 3.4 and Python 3.5 tests actually run correctly on
    Travis, and also adds Python 2.6 to the matrix.
    
    Fixes #76

--- travis/install-build-deps.sh
+++ travis/install-build-deps.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-sudo apt-get install autotools-dev g++ pkg-config
-
-if [ "$PYTHONVERSION" = "python3" ]; then
-    sudo apt-get install python3-dev
-else
-    sudo apt-get install python-dev
-fi
+sudo apt-get install autotools-dev g++ pkg-config python-dev python3-dev
