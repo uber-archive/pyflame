fd1a54c354f98667f545a53cfcca8bf3388ce18b
jmphilli <jphillips@lyft.com>
2017-07-13 16:01:04 -0700

    py3.4

--- src/pyfrob.cc
+++ src/pyfrob.cc
@@ -113,7 +113,7 @@ FROB_FUNCS
 }
 #endif
 
-#ifdef ENABLE_PY3
+#ifdef ENABLE_PY34
 namespace py3 {
 FROB_FUNCS
 }
@@ -147,7 +147,7 @@ void PyFrob::SetPython(PyVersion version) {
       get_threads_ = py2::GetThreads;
       break;
 #endif
-#ifdef ENABLE_PY3
+#ifdef ENABLE_PY34
     case PyVersion::Py3:
       get_threads_ = py3::GetThreads;
       break;
